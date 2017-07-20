eea9796ae341a863084bd206911007bc14030ce2
robert <robertndungu16@gmail.com>
2017-07-20 08:13:24 +0300

    [FEATURE]Add database connection configuration

--- config.py
+++ config.py
@@ -1,5 +1,21 @@
+import os
+import urllib
+import psycopg2
+
+
+urllib.parse.uses_netloc.append("postgres")
+url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
 SECRET_KEY = "mysecretalone"
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost/bucketlists'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost/bucketlists'
 CSRF_ENABLED = True
 USER_ENABLE_EMAIL = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
