61180a26c0305e7607f0efe9b429b9a84a3e5f7a
robert <robertndungu16@gmail.com>
2017-07-20 06:12:28 +0300

    [TASK]Add postgre database connection

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 SECRET_KEY = "mysecretalone"
-SQLALCHEMY_DATABASE_URI = ''
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost/bucketlists'
 CSRF_ENABLED = True
 USER_ENABLE_EMAIL = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
