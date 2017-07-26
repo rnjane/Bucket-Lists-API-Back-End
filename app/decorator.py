77af6bbb92d5af5f06e6a2a7194aca31edafb616
robert <robertndungu16@gmail.com>
2017-07-26 18:14:18 +0300

    [TASK]Add API decorator

---
+++ app/decorator.py
@@ -0,0 +1,20 @@
+import jwt
+import datetime
+from functools import wraps
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'token' in request.headers:
+            token = request.headers['token']
+        if not token:
+            return jsonify({'message' : 'Token is missing!'}), 401
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = User.query.filter_by(username=data['username']).first()
+        except:
+            return jsonify({'message' : 'Token is invalid!'}), 401
+        return f(current_user, *args, **kwargs)
+
+    return decorated
