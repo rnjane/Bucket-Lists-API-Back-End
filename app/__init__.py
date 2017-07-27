fdeb08988d235d1af03ab7ce3c0fe93e3e96f80d
robert <robertndungu16@gmail.com>
2017-07-27 21:10:07 +0300

    [BUG]Fix import issues

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from app import config
 
-
 app = Flask(__name__)
 app.config.from_object(config)
 db = SQLAlchemy(app)
