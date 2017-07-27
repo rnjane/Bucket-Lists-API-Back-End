d4405221486f2172627e1f9e2473f8fa62a5c54c
robert <robertndungu16@gmail.com>
2017-07-27 21:05:34 +0300

    [TESTS]Add items class tests

--- tests/test_items.py
+++ tests/test_items.py
@@ -1,86 +1,84 @@
 from tests.test_api import app, BucketListApiTest
 import json
 
+
 class ItemTest(BucketListApiTest):
     def test_itemadd_succes(self):
         '''test adding item works'''
         token = self.app.post('/auth/login', data=json.dumps(dict(
-                username='testuser1',
-                password='123456'
-            )),
+            username='testuser1',
+            password='123456'
+        )),
             content_type='application/json')
 
         data = json.loads(token.data.decode())
         tkn = data['token']
 
-        self.app.post('/bucketlists/bucketitem', headers=dict(
-                        token=[tkn]))
-
-        response = self.app.post("/bucketlists/bucketitem/items/", headers=dict(
-                                token=[tkn]), data=json.dumps(dict(
+        response = self.app.post("/bucketlists/2/items/", headers=dict(
+            token=[tkn]), data=json.dumps(dict(
                 itemname='testitem'
             )),
             content_type='application/json')
 
-        self.assertIn(b"Item added!", response.data)
+        self.assertEqual(response.status_code, 200)
 
     def test_itemview_succes(self):
         '''test viewing items work'''
         token = self.app.post('/auth/login', data=json.dumps(dict(
-                username='testuser1',
-                password='123456'
-            )),
+            username='testuser1',
+            password='123456'
+        )),
             content_type='application/json')
 
         data = json.loads(token.data.decode())
         tkn = data['token']
 
-        response = self.app.get("/bucketlists/bucketitem/items/", headers=dict(
+        response = self.app.get("/bucketlists/2/items/", headers=dict(
                                 token=[tkn]))
-        self.assertIn(b"testitem", response.data)
+        self.assertEqual(response.status_code, 200)
 
     def test_itemedit_succes(self):
         '''test item edit works'''
         token = self.app.post('/auth/login', data=json.dumps(dict(
-                username='testuser1',
-                password='123456'
-            )),
+            username='testuser1',
+            password='123456'
+        )),
             content_type='application/json')
 
         data = json.loads(token.data.decode())
         tkn = data['token']
 
         self.app.post("/bucketlists/bucketitem/items/", headers=dict(
-                                token=[tkn]), data=json.dumps(dict(
+            token=[tkn]), data=json.dumps(dict(
                 itemname='edtitem'
             )),
             content_type='application/json')
 
-        response = self.app.put('/bucketlists/bucketitem/items/edtitem', headers=dict(
+        response = self.app.put('/bucketlists/2/items/1', headers=dict(
                                 token=[tkn]), data=json.dumps(dict(
-                newname='neweditname',
-                status='Done'
-            )),
-            content_type='application/json')
-        self.assertIn(b'Item has been updated!', response.data)
+                                    newname='neweditname',
+                                    status='Done'
+                                )),
+                                content_type='application/json')
+        self.assertEqual(response.status_code, 200)
 
     def test_itemdelete_succes(self):
         '''test item delete works'''
         token = self.app.post('/auth/login', data=json.dumps(dict(
-                username='testuser1',
-                password='123456'
-            )),
+            username='testuser1',
+            password='123456'
+        )),
             content_type='application/json')
 
         data = json.loads(token.data.decode())
         tkn = data['token']
-        
+
         self.app.post("/bucketlists/bucketitem/items/", headers=dict(
-                                token=[tkn]), data=json.dumps(dict(
+            token=[tkn]), data=json.dumps(dict(
                 itemname='delitem'
             )),
             content_type='application/json')
 
-        response = self.app.delete('/bucketlists/bucketitem/items/delitem', headers=dict(
-                                token=[tkn]))
-        self.assertIn(b'Item deleted!', response.data)
+        response = self.app.delete('/bucketlists/1/items/1', headers=dict(
+            token=[tkn]))
+        self.assertEqual(response.status_code, 200)
