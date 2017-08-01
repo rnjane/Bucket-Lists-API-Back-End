32f04f9df67a214808b49bdd1ae89d3909c1c1b1
robert <robertndungu16@gmail.com>
2017-08-01 14:22:28 +0300

    [CHORE]Separate development, testing and production configs

--- app/config.py
+++ app/config.py
@@ -1,7 +1,32 @@
+class BaseConfig(object):
+    """Parent configuration class."""
+    DEBUG = False
+    CSRF_ENABLED = True
+    USER_ENABLE_EMAIL = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://rysxbcmtfrizgg:f83aa7c7c22300b62cd5cf475948efd146a33136c06b8029d44645a688e7d57c@ec2-184-73-236-170.compute-1.amazonaws.com/db7ai5ug00k7do'
+    SECRET_KEY = "mysecretalone"
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost/dbdemo'
-SECRET_KEY = "mysecretalone"
-#SQLALCHEMY_DATABASE_URI = 'postgresql://rysxbcmtfrizgg:f83aa7c7c22300b62cd5cf475948efd146a33136c06b8029d44645a688e7d57c@ec2-184-73-236-170.compute-1.amazonaws.com/db7ai5ug00k7do'
-CSRF_ENABLED = True
-USER_ENABLE_EMAIL = False
-SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+class TestConfig(BaseConfig):
+    """Configurations for Testing, with a separate test database."""
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost/testingdb'
+
+
+class DevelopmentConfig(BaseConfig):
+    """Configurations for Development."""
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    """Configurations for Production."""
+    DEBUG = False
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestConfig,
+    'production': ProductionConfig,
+}
