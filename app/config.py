2e02a6c08677cdce62c4e0496e20683846738dc5
Robert Ndungu <robert.ndungu@hotmail.com>
2017-08-18 17:32:13 +0300

    Update database URI

--- app/config.py
+++ app/config.py
@@ -4,8 +4,9 @@ class BaseConfig(object):
     CSRF_ENABLED = True
     USER_ENABLE_EMAIL = False
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://rysxbcmtfrizgg:f83aa7c7c22300b62cd5cf475948efd146a33136c06b8029d44645a688e7d57c@ec2-184-73-236-170.compute-1.amazonaws.com/db7ai5ug00k7do'
     # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost/bucketlists'
-    SQLALCHEMY_DATABASE_URI = 'postgres://rysxbcmtfrizgg:f83aa7c7c22300b62cd5cf475948efd146a33136c06b8029d44645a688e7d57c@ec2-184-73-236-170.compute-1.amazonaws.com:5432/db7ai5ug00k7do'
+    # SQLALCHEMY_DATABASE_URI = 'postgres://rysxbcmtfrizgg:f83aa7c7c22300b62cd5cf475948efd146a33136c06b8029d44645a688e7d57c@ec2-184-73-236-170.compute-1.amazonaws.com:5432/db7ai5ug00k7do'
     SECRET_KEY = "mysecretalone"
 
 
