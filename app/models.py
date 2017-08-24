2099576c8b205258a564bab32296059114575e8c
Robert Ndungu <robert.ndungu@hotmail.com>
2017-08-24 20:53:03 +0300

    [task]Add fields to the registration table

--- app/models.py
+++ app/models.py
@@ -3,6 +3,9 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False, server_default='')
     active = db.Column(db.Boolean(), nullable=False, server_default='0')
