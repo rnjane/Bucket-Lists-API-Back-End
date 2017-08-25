524fb6be68afe6b78eebdb7c550bfbe468fb9bb8
Robert Ndungu <robert.ndungu@hotmail.com>
2017-08-25 18:47:57 +0300

    Update test database

--- app/config.py
+++ app/config.py
@@ -4,9 +4,7 @@ class BaseConfig(object):
     CSRF_ENABLED = True
     USER_ENABLE_EMAIL = False
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://rysxbcmtfrizgg:f83aa7c7c22300b62cd5cf475948efd146a33136c06b8029d44645a688e7d57c@ec2-184-73-236-170.compute-1.amazonaws.com/db7ai5ug00k7do'
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost/bucketlists'
-    # SQLALCHEMY_DATABASE_URI = 'postgres://rysxbcmtfrizgg:f83aa7c7c22300b62cd5cf475948efd146a33136c06b8029d44645a688e7d57c@ec2-184-73-236-170.compute-1.amazonaws.com:5432/db7ai5ug00k7do'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost/bucketlists'
     SECRET_KEY = "mysecretalone"
 
 
@@ -14,7 +12,7 @@ class TestConfig(BaseConfig):
     """Configurations for Testing, with a separate test database."""
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgres://flrzohivgsrpdf:3c152588142962a5363d2021dc68d240078b76c4b3bd7f029bbc3b080293520b@ec2-54-163-254-143.compute-1.amazonaws.com:5432/d2b81roptb5nml'
+    SQLALCHEMY_DATABASE_URI = 'postgres://gvbgebgkmbusei:e705e80ca127b5a0a5f4b107b49e2a2b3c45037d49514655b633694405a54bc0@ec2-107-20-250-195.compute-1.amazonaws.com:5432/d67pqgge6unh4g'
 
 
 class DevelopmentConfig(BaseConfig):
