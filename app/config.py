7dd59b1950fce1d2db4d16f1653b3aa037ba5ed8
Robert Ndungu <robert.njane@andela.com>
2017-11-08 09:47:58 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ class BaseConfig(object):
     CSRF_ENABLED = True
     USER_ENABLE_EMAIL = False
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = 'postgres://rysxbcmtfrizgg:f83aa7c7c22300b62cd5cf475948efd146a33136c06b8029d44645a688e7d57c@ec2-184-73-236-170.compute-1.amazonaws.com:5432/db7ai5ug00k7do'
+    SQLALCHEMY_DATABASE_URI = 'postgres://gvbgebgkmbusei:e705e80ca127b5a0a5f4b107b49e2a2b3c45037d49514655b633694405a54bc0@ec2-107-20-250-195.compute-1.amazonaws.com:5432/d67pqgge6unh4g'
     SECRET_KEY = "mysecretalone"
 
 
